31e5dd79d2e20056263053f1b4f94280536b4bdc
rosasian <rosasian@rosasianui-MacBookAir.local>
2024-12-12 17:25:28 +0900

    init commit

---
+++ src/main/java/com/example/wedlessInvite/WedlessInviteApplication.java
@@ -0,0 +1,11 @@
+package com.example.wedlessInvite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedlessInviteApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WedlessInviteApplication.class, args);
+	}
+}

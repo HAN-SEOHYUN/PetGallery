b92e71ad9228cf589b42a885d703df7be4ab1173
HANSEOHYUN <000312@daum.net>
2024-12-18 10:36:40 +0900

    S3UploadService -> S3FileService 변경

--- src/main/java/com/example/wedlessInvite/controller/S3UploadController.java
+++ src/main/java/com/example/wedlessInvite/controller/S3UploadController.java
@@ -2,7 +2,7 @@ package com.example.wedlessInvite.controller;
 
 import com.example.wedlessInvite.dto.ImageUploadDto;
 import com.example.wedlessInvite.service.ImageUploadService;
-import com.example.wedlessInvite.service.S3UploadService;
+import com.example.wedlessInvite.service.S3FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,14 +16,14 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class S3UploadController {
 
-    private final S3UploadService s3UploadService;
+    private final S3FileService s3FileService;
     private final ImageUploadService imageUploadService;
 
     @PostMapping("/s3/upload")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
         try {
             // 파일을 S3에 저장하고 URL을 반환
-            ImageUploadDto dto = s3UploadService.uploadS3(file);
+            ImageUploadDto dto = s3FileService.uploadS3(file);
             imageUploadService.saveFile(dto);
             return ResponseEntity.ok().body(dto.getOrgFileName());
 
--- src/main/java/com/example/wedlessInvite/service/S3FileService.java
+++ src/main/java/com/example/wedlessInvite/service/S3FileService.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class S3UploadService {
+public class S3FileService {
 
     private final AmazonS3 amazonS3;
 

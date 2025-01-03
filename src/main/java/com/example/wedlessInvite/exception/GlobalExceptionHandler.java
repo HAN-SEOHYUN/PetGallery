b93296822f5d870fd7affc00ba3ac7661f339098
HANSEOHYUN <000312@daum.net>
2025-01-03 20:03:16 +0900

    [task] API 요청 시 필수 필드 검증 resolve  #59

--- src/main/java/com/example/wedlessInvite/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/wedlessInvite/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.example.wedlessInvite.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,5 +17,20 @@ public class GlobalExceptionHandler {
                 ex.getErrorCode().getErrorMsg()
         );
         return new ResponseEntity<>(errorResponse, ex.getErrorCode().getStatusCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponseDto> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        StringBuilder errorMsg = new StringBuilder("필수입력값 누락: ");
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            errorMsg.append(fieldError.getDefaultMessage())
+                    .append("; ");
+        }
+
+        ErrorResponseDto errorResponse = new ErrorResponseDto(
+                HttpStatus.BAD_REQUEST,
+                errorMsg.toString()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }

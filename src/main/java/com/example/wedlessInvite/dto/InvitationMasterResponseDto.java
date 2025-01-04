304111d56d2a52e918280dc3893158be6592509e
HANSEOHYUN <000312@daum.net>
2025-01-04 20:43:03 +0900

    [task] 등록된 청첩장 리스트 조회 API 개발 resolve #63

---
+++ src/main/java/com/example/wedlessInvite/dto/InvitationMasterResponseDto.java
@@ -0,0 +1,32 @@
+package com.example.wedlessInvite.dto;
+
+import com.example.wedlessInvite.domain.Invitation.BrideInfo;
+import com.example.wedlessInvite.domain.Invitation.GroomInfo;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class InvitationMasterResponseDto {
+    private Long id;
+    private LocalDate date;
+    private BrideInfo brideInfo;
+    private GroomInfo groomInfo;
+    private String letterTxt;
+    private String mainTxt;
+    private String greetTxt;
+
+    @Builder
+    public InvitationMasterResponseDto(Long id, LocalDate date, BrideInfo brideInfo, GroomInfo groomInfo, String letterTxt, String mainTxt, String greetTxt) {
+        this.id = id;
+        this.date = date;
+        this.brideInfo = brideInfo;
+        this.groomInfo = groomInfo;
+        this.letterTxt = letterTxt;
+        this.mainTxt = mainTxt;
+        this.greetTxt = greetTxt;
+    }
+}

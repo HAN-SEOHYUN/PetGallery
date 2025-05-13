557695f864ed59b8988bfab8456e6508e4222a4c
HANSEOHYUN <000312@daum.net>
2025-05-13 16:34:39 +0900

    ``` feat: 회원가입 로직에 유효성 검사 및 경고 추가
    회원가입 시 비밀번호 확인 로직과 경고 메시지를 추가함.
    회원가입 성공 및 실패에 따른 알림과 리다이렉트 처리 구현.
    ```

---
+++ src/main/resources/static/js/pages/register/app.js
@@ -0,0 +1,97 @@
+const REQUEST_URL = '/api/register';
+const LOGIN_PAGE = '/main';
+
+$(document).ready(function () {
+
+    $('#registerForm').on('submit', function (e) {
+        e.preventDefault();
+
+        if (!isPasswordConfirmed()) {
+            alert('비밀번호가 일치하지 않습니다.');
+            return;
+        }
+
+        showTwoButtonAlert({
+            title: '가입하시겠습니까 ?',
+            text: '',
+            alertType: 'info',
+            callback() {
+                addData(REQUEST_URL, getRequestData());
+            }
+        });
+    });
+
+    function getRequestData() {
+        const name = $('#name').val().trim();
+        const password = $('#password').val();
+
+        return {
+            name: name,
+            password: password
+        };
+    }
+
+    /**
+     * 이미지 데이터를 서버에 전송하여 등록하는 함수.
+     *
+     * @param {String} url - 데이터를 전송할 API URL.
+     * @param {Object} requestData - 전송할 요청 데이터. JSON 형식으로 변환되어 서버로 전송됩니다.
+     *
+     */
+    function addData(url, requestData) {
+        fetchData(url, {
+            method: HTTP_METHODS.POST,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: JSON.stringify(requestData)
+        })
+            .then(response => { // 성공
+                showSuccessAndRedirectAlert(LOGIN_PAGE);
+            })
+            .catch(error => { // 실패
+                handleSubmitErrorResponse(JSON.parse(error));
+            });
+    }
+
+    function isPasswordConfirmed() {
+        const password = $('#password').val();
+        const confirmPassword = $('#confirm-password').val();
+
+        return password === confirmPassword;
+    }
+});
+
+/**
+ * 가입 완료 후 알림을 표시하고, 알림이 닫히면 리다이렉트하는 함수
+ *
+ * @param {string} redirectUrl - 리다이렉트할 주소 (예: '/pages/main')
+ */
+function showSuccessAndRedirectAlert(redirectUrl) {
+    showOneButtonAlert({
+        title: '등록되었습니다.',
+        alertType: 'success',
+        callback() {
+            window.location.href = redirectUrl;
+        }
+    });
+}
+
+/**
+ * 가입 실패 시 실패 사유를 알려주는 함수
+ *
+ * @param {JSON} error - API에서 응답받은 데이터
+ */
+function handleSubmitErrorResponse(error) {
+    if (error.statusCode === HTTP_STATUS.BAD_REQUEST) {
+        showOneButtonAlert({
+            title: '가입에 실패했습니다',
+            text: error.message,
+            alertType: 'error',
+            callback() {
+            }
+        });
+    } else {
+        throw error;
+    }
+}

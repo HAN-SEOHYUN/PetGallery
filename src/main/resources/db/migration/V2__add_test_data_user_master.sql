3ce764d9a23a48598533e3564e079eff4c5b21d4
HANSEOHYUN <000312@daum.net>
2025-05-29 15:49:25 +0900

    ``` delete : Comment out SQL statements in migration scripts ```

--- src/main/resources/db/migration/V2__add_test_data_user_master.sql
+++ src/main/resources/db/migration/V2__add_test_data_user_master.sql
@@ -1,3 +1,3 @@
-INSERT INTO user_master
-(mod_time, reg_time, um_id, created_by, last_modified_by, um_name, um_pwd)
-VALUES('2025-05-12 07:22:12.523370', '2025-05-12 07:22:12.523370', 1, NULL, NULL, '로사', '1234');
+# INSERT INTO user_master
+# (mod_time, reg_time, um_id, created_by, last_modified_by, um_name, um_pwd)
+# VALUES('2025-05-12 07:22:12.523370', '2025-05-12 07:22:12.523370', 1, NULL, NULL, '로사', '1234');

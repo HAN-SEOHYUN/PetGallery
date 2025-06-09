cd120356fd3b741257b284ce2e8778ed8346d8fb
HANSEOHYUN <000312@daum.net>
2025-06-09 14:13:47 +0900

    [css] 메인 페이지 로그인 안내 문구 추가 resolve #222

--- src/main/resources/db/migration/V3__add_constraint_on_cascade.sql
+++ src/main/resources/db/migration/V3__add_constraint_on_cascade.sql
@@ -1,5 +1,5 @@
-ALTER TABLE image_uploads DROP FOREIGN KEY FKrxc9u2h2v49lo9daoh55lebpu;
-ALTER TABLE image_uploads
-    ADD CONSTRAINT FKrxc9u2h2v49lo9daoh55lebpu
-        FOREIGN KEY (iu_pm_id) REFERENCES pet_master(pm_id)
-            ON DELETE SET NULL;
+# ALTER TABLE image_uploads DROP FOREIGN KEY FKrxc9u2h2v49lo9daoh55lebpu;
+# ALTER TABLE image_uploads
+#     ADD CONSTRAINT FKrxc9u2h2v49lo9daoh55lebpu
+#         FOREIGN KEY (iu_pm_id) REFERENCES pet_master(pm_id)
+#             ON DELETE SET NULL;
